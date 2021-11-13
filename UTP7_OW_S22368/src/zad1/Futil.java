2952d56fa8c85d5ebb7d1cbee2d51f83e444a532
wojtekolesinski <wojtek.olesinski00@gmail.com>
2021-11-13 20:10:22 +0100

    UTP 7

--- UTP7_OW_S22368/src/zad1/Futil.java
+++ UTP7_OW_S22368/src/zad1/Futil.java
@@ -1,65 +1,38 @@
 package zad1;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.util.stream.Stream;
 
 public class Futil {
 		
 	public static void processDir(String dirname, String resultFileName) {
-		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(resultFileName), StandardCharsets.UTF_8, StandardOpenOption.APPEND)){
-			Files.walk(Paths.get(dirname))
+		try {
+			Stream<String> lines = Files.walk(Paths.get(dirname))
 				.filter(Files::isRegularFile)
 				.filter(path -> path.toString().endsWith("txt"))
-				.forEachOrdered(path -> {
-					try (BufferedReader reader = Files.newBufferedReader(path, Charset.forName("windows-1250"))) {
-						reader.lines().forEachOrdered(line -> {
-							try {
-								writer.write(line + "\n");
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-						});
+				.flatMap(path -> {
+					try {
+						return Files.lines(path, Charset.forName("windows-1250"));
 					} catch (IOException e) {
 						e.printStackTrace();
+						return null;
 					}
-				});
-		} catch (NoSuchFileException e){	
-			try {
-				Files.createFile(Paths.get(resultFileName));
-			} catch (IOException e1) {
-				e1.printStackTrace();
+				})
+				.filter(line -> line != null);
+			
+			Path resultPath = Paths.get("./" + resultFileName);
+			if (!Files.exists(resultPath)) {
+				Files.createFile(resultPath);
 			}
+			Files.write(resultPath, (Iterable<String>)lines::iterator, StandardCharsets.UTF_8);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	/*
-	 * try ( BufferedWriter writer = Files.newBufferedWriter(
-	 * Paths.get(resultFileName), StandardCharsets.UTF_8, StandardOpenOption.APPEND)
-	 * ) {
-	 * 
-	 * File baseDir = new File(dirname);
-	 * 
-	 * for (File file: baseDir.listFiles()) { if (file.isDirectory()) {
-	 * processDir(file.getAbsolutePath(), resultFileName); } else { try
-	 * (BufferedReader reader = Files.newBufferedReader(file.toPath(),
-	 * Charset.forName("windows-1250"))) { String line; while((line =
-	 * reader.readLine()) != null) { writer.write(line); } } catch (IOException e) {
-	 * // TODO Auto-generated catch block e.printStackTrace(); } } }
-	 * 
-	 * } catch (IOException e1) { // TODO Auto-generated catch block
-	 * e1.printStackTrace(); }
-	 */
+
 }
--- UTP7_OW_S22368/src/zad2/Main.java
+++ UTP7_OW_S22368/src/zad2/Main.java
@@ -6,8 +6,35 @@
 
 package zad2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+	  Map<String, List<String>> map = new BufferedReader(new InputStreamReader(new URL("http://wiki.puzzlers.org/pub/wordlists/unixdict.txt").openStream()))
+	  															.lines()
+																.collect(Collectors.groupingBy(str -> {
+																	char[] arr = str.toCharArray();
+																	Arrays.sort(arr);
+																	return String.copyValueOf(arr);
+																}, LinkedHashMap::new, Collectors.toList()));
+	  	  
+	  map.values()
+	  		.stream()
+			.filter(e -> e.size() == map.values()
+										.stream()
+										.mapToInt(v -> v.size())
+										.max()
+										.getAsInt())
+			.forEach(e -> System.out.println(e.stream().collect(Collectors.joining(" "))));
+
   }
 }
