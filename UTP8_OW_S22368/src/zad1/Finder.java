30aad442dfae63b1d8b0c7cc9ca498487109da6e
wojtekolesinski <wojtek.olesinski00@gmail.com>
2021-11-20 17:47:09 +0100

    UTP8

---
+++ UTP8_OW_S22368/src/zad1/Finder.java
@@ -0,0 +1,66 @@
+/**
+ *
+ *  @author Olesiński Wojciech S22368
+ *
+ */
+
+package zad1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Finder {
+	
+	private String fileName;
+	
+	public Finder(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	public int getIfCount() {
+		Pattern pattern = Pattern.compile("if");
+		int ifCount = 0;
+		try {
+			for (String line: Files.readAllLines(Path.of(fileName))) {
+				Matcher matcher = pattern.matcher(line);
+				
+				while (matcher.find()) {
+					ifCount++;
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return ifCount;
+	}
+	
+	public int getStringCount(String string) {
+		Pattern pattern = Pattern.compile(string);
+		int ifCount = 0;
+		try {
+			for (String line: Files.readAllLines(Path.of(fileName))) {
+				Matcher matcher = pattern.matcher(line);
+				
+				while (matcher.find()) {
+					ifCount++;
+				}
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return ifCount;
+	}
+	
+	
+}    
