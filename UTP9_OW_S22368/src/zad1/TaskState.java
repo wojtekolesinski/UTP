cb2d76bc79fc127b445a7c7fa76bf1ffc92f65d8
wojtekolesinski <wojtek.olesinski00@gmail.com>
2021-11-27 22:21:07 +0100

    UTP 9.1

---
+++ UTP9_OW_S22368/src/zad1/TaskState.java
@@ -0,0 +1,8 @@
+package zad1;
+
+public enum TaskState {
+	CREATED,
+	RUNNING,
+	ABORTED,
+	READY
+}
---
+++ UTP9_OW_S22368/src/zad2/Main.java
@@ -0,0 +1,14 @@
+/**
+ *
+ *  @author Olesiński Wojciech S22368
+ *
+ */
+
+package zad2;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+  }
+}
